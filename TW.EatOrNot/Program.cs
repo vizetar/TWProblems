9a5d37af96ba29b15a7a40197314f7ce6d230032
vizetar <vijetatomar22@gmail.com>
2018-07-26 12:45:18 +0530

    commit basic architecture

---
+++ TW.EatOrNot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TW.EatOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+        }
+    }
+}
---
+++ TW.MillyAndRank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TW.MillyAndRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
